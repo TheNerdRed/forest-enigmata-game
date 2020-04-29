9bd976bc61398ee25fd3a5fc2a58a32d10b0d1a4
Lewis Binnie <lewis.binnie@outlook.com>
2020-04-29 20:54:17 +0100

    New forest layout and modular room generation

---
+++ Forest Enigmata/Assets/Scripts/RoomChooser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomChooser : MonoBehaviour
+{
+    [SerializeField] 
+    
+    private GameObject NextRoom;
+
+    [SerializeField]
+    private GameObject destroyThisRoom;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(destroyThisRoom);
+        Instantiate(NextRoom);
+    }
+    
+}
---
+++ Forest Enigmata/Assets/Scripts/TokenCollection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TokenCollection : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player has collided with token!");
+            Destroy(gameObject);
+        }
+    }
+}
