46757f5ff7970142d7e7e30253404bda20fafdef
Lewis Binnie <lewis.binnie@outlook.com>
2020-05-04 10:38:50 +0100

    Added course assets and token system.
    - Added CwCl assets to folders.
    - Deleted old Prototype assets.
    - Added tokens to map.

---
+++ Forest Enigmata/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    public int tokenScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialises the score to 0 and resets the text counter.
+        tokenScore = 0;
+        scoreText.text = "Score : " + tokenScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If r is pressed, restart the game.
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            restartGame();
+        }
+    }
+     
+    // When this method is called, it restarts the game and loads from scratch.
+    void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // When this method is called and passed a parameter of the score to update, it updates by that amount.
+    public void updateScore(int scoreToAdd)
+    {
+        tokenScore += scoreToAdd;
+        scoreText.text = "Score : " + tokenScore;
+    }
+}
--- Forest Enigmata/Assets/Scripts/TokenCollection.cs
+++ Forest Enigmata/Assets/Scripts/TokenCollection.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class TokenCollection : MonoBehaviour
 {
+    // References the Game Manager class, so methods from within can be used.
+    public GameManager gameManager;
+
     public GameObject player;
+
+    public int minimumScore = 3;
+    public int maximumScore = 9;
+    public int randomScore;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         player = GameObject.Find("Player");
     }
 
@@ -22,7 +30,9 @@ public class TokenCollection : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Player has collided with token!");
+            randomScore = Random.Range(minimumScore, maximumScore);
+
+            gameManager.updateScore(randomScore);
             Destroy(gameObject);
         }
     }
